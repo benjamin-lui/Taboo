a09688214f54dd261fedd24d2b438321ac2c610d
Benjamin Lui <benlui@berkeley.edu>
2012-07-14 09:41:18 -0700

    added transitionviewcontroller

---
+++ Taboo/TransitionViewController.h
@@ -0,0 +1,13 @@
+//
+//  TransitionViewController.h
+//  Taboo
+//
+//  Created by Benjamin Lui on 7/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransitionViewController : UIViewController
+
+@end
